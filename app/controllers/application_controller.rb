24675e72a5bd316f3246c1c2eb9a575051456ae2
vinibol12 <vinisem@gmail.com>
2017-04-18 19:38:44 +1200

    Add devise user model and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name, :password_confirmation]
+    devise_parameter_sanitizer.for(:sign_in) << [:email, :remember_me]
+  end
 end
